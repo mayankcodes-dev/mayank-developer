082e21102ac74e2f3b1259dec64861246dafed29
coderMayank.69 <mayankbca96325@gmail.com>
2026-05-01 23:52:49 +0530

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <PageLoader />
         <ThemeProvider>{children}</ThemeProvider>
       </body>
